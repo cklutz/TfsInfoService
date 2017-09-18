ab764f90c4b1834eb23017adc245546f1dfde13d
Christian Klutz <cklutz@gmail.com>
2017-09-18 09:08:41 +0200

    added result-ago type

--- TfsInfoService/Controllers/InfosController.cs
+++ TfsInfoService/Controllers/InfosController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Common.Internal;
 using TfsInfoService.Utilities;
 
 namespace TfsInfoService.Controllers
@@ -24,12 +26,12 @@ namespace TfsInfoService.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new[] { "buildnumber", "duration", "finishdate", "coverage", "best-coverage", "custom" };
+            return new[] { "result-age", "buildnumber", "duration", "finishdate", "coverage", "best-coverage", "custom" };
         }
 
         [HttpGet("{teamProject}/{buildDefinitionId}/{type}/badge")]
         public async Task<IActionResult> GetBadge(Guid teamProject, int buildDefinitionId, string type,
-            string title, string titlefg, string titlebg, 
+            string title, string titlefg, string titlebg,
             string value, string valuefg, string valuebg,
             string subType)
         {
@@ -63,6 +65,10 @@ namespace TfsInfoService.Controllers
                             title = title ?? "number";
                             value = build.BuildNumber;
                             break;
+                        case "result-age":
+                            title = title ?? "build";
+                            value = GetResultAge(subType, build, ref valuebg, ref valuefg);
+                            break;
                         case "duration":
                             title = title ?? "duration";
                             value = GetDuration(build);
@@ -88,6 +94,65 @@ namespace TfsInfoService.Controllers
             }
             var doc = BadgeGenerator.CreateSvgBadge(title, titlefg, titlebg, value, valuefg, valuebg);
             return Content(doc.ToString(), "image/svg+xml");
+        }
+
+        private static string GetResultAge(string subType, Build build, ref string valuebg, ref string valuefg)
+        {
+            string value;
+
+            if (!build.FinishTime.HasValue)
+            {
+                valuefg = "#fff";
+                valuebg = "#2E64FE";
+                if (build.StartTime.HasValue)
+                {
+                    value = "started " + build.StartTime.Value.Ago();
+                }
+                else
+                {
+                    value = "in progress";
+                }
+            }
+            else
+            {
+                string displayResult;
+                switch (build.Result.GetValueOrDefault())
+                {
+                    case BuildResult.Succeeded:
+                        valuefg = "#fff";
+                        valuebg = "#4BAE4F";
+                        displayResult = "succeeded";
+                        break;
+                    case BuildResult.PartiallySucceeded:
+                        valuefg = "#000";
+                        valuebg = "#FEC006";
+                        displayResult = "partially succeeded";
+                        break;
+                    case BuildResult.Failed:
+                        valuefg = "#fff";
+                        valuebg = "#F34235";
+                        displayResult = "failed";
+                        break;
+                    case BuildResult.Canceled:
+                        valuefg = "#fff";
+                        valuebg = "#F34235";
+                        displayResult = "canceled";
+                        break;
+                    default:
+                        valuefg = "#fff";
+                        valuebg = "#BBBBBB";
+                        displayResult = "none";
+                        break;
+                }
+
+                value = build.FinishTime.Value.Ago();
+
+                if ("result-label".Equals(subType, StringComparison.OrdinalIgnoreCase))
+                {
+                    value = displayResult + " " + value;
+                }
+            }
+            return value;
         }
 
         private async Task<string> GetCoverageAsync(Guid teamProject, string subType, Build build)
